820456ac897ba68698f274f73d06abed3cc11e0f
Nadja <nadja.planic@edu.fham.de>
2020-07-16 20:11:53 +0200

    Stack + Queue

--- src/test/java/de/fham/softwareentwicklung/zwei/queue/StackTest.java
+++ src/test/java/de/fham/softwareentwicklung/zwei/queue/StackTest.java
@@ -1,7 +1,8 @@
 package de.fham.softwareentwicklung.zwei.queue;
 
 import jdk.swing.interop.SwingInterOpUtils;
-
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.*;
 import java.util.logging.StreamHandler;
 
@@ -10,6 +11,8 @@ class StackTest
     public static void main(String[] args)
     {
         Stack s=new Stack();
+
+        Queue q= new LinkedList();
 
         System.out.println("Add " + s.push("Red"));
         System.out.println("Add " + s.push("Orange"));
@@ -22,6 +25,8 @@ class StackTest
 
         System.out.println("Colors of the Rainbow" + s);
 
+
+
         System.out.println("Remove "+ s.pop());
         System.out.println("Remove "+ s.pop());
         System.out.println("Remove "+ s.pop());
@@ -32,5 +37,32 @@ class StackTest
 
         System.out.println("What Colors are left?" + s);
 
+        q.add("Strawberries");
+        q.add("Blueberries");
+        q.add("Raspberries");
+        q.add("Kiwis");
+        q.add("Orange");
+        q.add("Apples");
+        q.add("Mango");
+        q.add("Grapes");
+
+        System.out.println("Now it is a Fruit Salad: ");
+        Iterator itr= q.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+        System.out.println("I ate in FIFO Order:");
+        System.out.println("First " + q.remove());
+        System.out.println("Second " + q.remove());
+        System.out.println("Third " + q.remove());
+        System.out.println("Fourth " + q.remove());
+        System.out.println("Fifth " + q.remove());
+        System.out.println("Sixth " + q.remove());
+        System.out.println("Seventh " + q.remove());
+        System.out.println("Finally the " + q.remove());
+
+        }
+
     }
-}
+
